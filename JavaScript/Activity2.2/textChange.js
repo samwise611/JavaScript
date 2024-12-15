9d257ea26392ef5354a6ac87b308d1f1a3fc3d4f
samwise611 <samwise61@googlemail.com>
2024-12-15 19:58:40 +0000

    Add files via upload

--- JavaScript/Activity2.2/textChange.js
+++ JavaScript/Activity2.2/textChange.js
@@ -1,4 +1,6 @@
 function textChange() {
     var heading = document.getElementById("demo")
     heading.textContent = "New Text";
+    heading.style.color = "blue";
+    heading.style.fontSize = "40px";
 }
